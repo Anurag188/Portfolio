b9a9097f8eea4eadc91ce2a17b259342a3af9a2c
anurag agarwal <anuragagarwal188@gmail.com>
2024-03-06 19:26:07 +0530

    final

--- components/home.tsx
+++ components/home.tsx
@@ -5,7 +5,7 @@ import { IoMdArrowRoundForward } from "react-icons/io";
 const Home = () => {
     return (
         <div className="flex flex-col w-full h-full md:flex-row items-center justify-center overflow-auto">
-            <div className="md:bg-[#ffb400] h-[0px] md:h-[200%] w-[100%] left-[-83%] top-[-50%] fixed rotate-[-15deg]"></div>
+            <div className="md:bg-[#ffb400] h-[0px] w-[0px] md:h-[200%] md:w-[100%] left-[-83%] top-[-50%] fixed rotate-[-15deg]"></div>
             <div className="z-10 md:h-full mt-8 md:mt-0 md:p-8">
                 <div className="bg-black"></div>
                 <Image src="/pic.jpg" alt="" width="200" height="300" className="w-[200px] h-[200px] object-scale-down md:object-cover md:min-w-[300px] md:w-[1100px] md:h-[900px] ml-8 md:rounded-3xl rounded-full border-4 border-[#292929] md:border-none bg-black" />
@@ -17,7 +17,7 @@ const Home = () => {
                     I am software & full-stack web developer focused on crafting clean & user-friendly experiences, I am passionate about building excellent software that improves the lives of those around me.
                 </div>
 
-                <button className="mt-[40px] flex items-center justify-between w-[230px] pl-6 h-[50px] md:mt-4 text-[15px] text-gray-600 dark:text-white rounded-full transition duration-500 ease-out hover:bg-[#ffb400] border-2 border-[#ffb400]">
+                <button className="mt-[20px] flex items-center justify-between w-[230px] pl-6 h-[50px] md:mt-4 text-[15px] text-gray-600 dark:text-white rounded-full transition duration-500 ease-out md:hover:bg-[#ffb400] border-2 border-[#ffb400]">
                     MORE ABOUT ME
                     <IoMdArrowRoundForward size={20} className="text-white h-[50px] w-[50px] bg-[#ffb400] rounded-full" />
                 </button>
